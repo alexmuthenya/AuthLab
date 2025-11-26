5b40df075c07fc73779b6abfbac5c6cd35bae57b
Alex <alexmuthenya100@gmail.com>
2025-11-26 15:37:38 +0300

    feat: initialize Spring Boot project structure

---
+++ src/test/java/com/alex/AuthLab/AuthLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.AuthLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
