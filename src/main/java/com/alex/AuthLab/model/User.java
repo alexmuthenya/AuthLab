86c791ec13cd6324a5a8581a8714679080ed8d07
Alex <alexmuthenya100@gmail.com>
2025-12-17 17:57:18 +0300

    Refactor User model: add Lombok, secure toString, and safe equals/hashCode

--- src/main/java/com/alex/AuthLab/model/User.java
+++ src/main/java/com/alex/AuthLab/model/User.java
@@ -1,54 +1,23 @@
 package com.alex.AuthLab.model;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "users")
+@ToString(exclude = "password")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
 public class User {
     @Id
+    @EqualsAndHashCode.Include
     private String id;
+
     private String email;
-    private String username;
     private String password;
-    public  User(){}
-    public User(String id, String email, String password, String username) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.username = username;
-    }
 
-
-    public String getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
